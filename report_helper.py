e135da3e26ca5e1b5e8fb0f3632141c58add88f4
Heimd411 <heimd411@localhost.localdomain>
2024-01-22 20:49:59 +0100

    sudo code

---
+++ report_helper.py
@@ -0,0 +1,12 @@
+
+screenshooter():
+    save screenshot to $file.docx
+
+make_comment():
+    add comment to image in $file.docx
+    user input()
+
+main("save file"):
+    if screenshot:
+        screenshooter()
+        make_comment()
